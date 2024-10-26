bebb907cea5f85a6c77e2342db85060fc4cfa486
Phavour-EBEN <ainooebenezer05@gmail.com>
2024-10-26 01:24:06 +0000

    Adding draw.py file that contains some skills

--- draw.py
+++ draw.py
@@ -9,7 +9,21 @@ cv.imshow('blank', blank)
 # cv.imshow('blank-green', blank)
 
 # drawing a rectangle on the plane
+'''
 cv.rectangle(blank, (0,0), (250,250),(0,255,0), thickness=2)
 cv.imshow('rectangle', blank)
+'''
+'''
+# Trying to draw a circle
+cv.circle(blank,
+          center=(blank.shape[0]//2,
+          blank.shape[1]//2), 
+          radius=50, 
+          color=(0,255,0), 
+          thickness=1)
+
+cv.imshow('circle', blank)
+'''
+# Adding text to a frame
 
 cv.waitKey(0)
