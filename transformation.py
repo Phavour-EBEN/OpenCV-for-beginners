02e4eaf312c310894516050f743b93b6060b2076
Phavour-EBEN <ainooebenezer05@gmail.com>
2024-10-26 16:43:25 +0000

    Image translation walk through

---
+++ transformation.py
@@ -0,0 +1,27 @@
+import cv2 as cv
+import numpy as np
+
+img = cv.imread('photos\photo_5878501912922403677_x.jpg')
+cv.imshow("Image", img)
+
+# Cropping function
+crop_img = img[50:250, 0:250]
+cv.imshow("Cropped Image", crop_img)
+
+# Transformation
+def img_tranformatioN(img, x, y):
+    matrix = np.float32([[1,0,x], [0,1,y]])
+    dimensions = (img.shape[1], img.shape[0])
+    translate = cv.warpAffine(img, matrix, dimensions)
+
+    return translate
+'''
+note: 
+-x--left
+x---right
+-y--up
+y---down
+'''
+trans_img = img_tranformatioN(img, 250,250)
+cv.imshow("Translated Image", trans_img)
+cv.waitKey(0)
